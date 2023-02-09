33bb2e503553b2c3ae3a66115fd255e9178c0bc1
Netkas <zixing.narrakas1996@gmail.com>
2023-02-09 17:10:38 -0500

    Added \TamerLib\Abstracts > ObjectType

---
+++ src/TamerLib/Abstracts/ObjectType.php
@@ -0,0 +1,12 @@
+<?php
+
+    namespace TamerLib\Abstracts;
+
+    abstract class ObjectType
+    {
+        const Job = 'tamer_job';
+
+        const JobResults = 'tamer_job_results';
+
+        const Unknown = 'unknown';
+    }
